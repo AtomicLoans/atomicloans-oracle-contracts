a77a7c8795699a12c40c26ddac84d5ced22be3d9
Matthew Black <mblackmblackmblack@gmail.com>
2019-07-23 12:23:29 -0400

    Add truffle config for coveralls

--- truffle.js
+++ truffle.js
@@ -8,6 +8,13 @@ module.exports = {
       port: 8545,
       network_id: "*"
     },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
     goerli: {
       provider: function() {
         return new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/53bcde36e0404a6da87b71e780783f79")
