6896b8242d77d479a9b2136f0824229bebfd90a5
Matthew Black <mblackmblackmblack@gmail.com>
2019-08-01 20:04:13 -0400

    Exclude oraclize and chainlink from coveralls report

---
+++ .solcover.js
@@ -0,0 +1,12 @@
+module.exports = {
+  skipFiles: [
+    'DSMath.sol',
+    'Buffer.sol',
+    'WETH9.sol',
+    'DSValue.sol',
+    'chainlink/Chainlinked.sol',
+    'chainlink/ChainlinkedTesting.sol',
+    'oraclize/OraclizeAPI.sol',
+    'oraclize/OraclizeAPITesting.sol'
+  ]
+};
